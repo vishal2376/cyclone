a3286c7c207509edee0b5eac649a48b6163b4f75
Vishal Singh <vishalsingh2376@gmail.com>
2023-04-09 15:57:12 +0530

    home page done

--- app/src/main/java/com/vishal2376/cyclone/fragments/HomeFragment.kt
+++ app/src/main/java/com/vishal2376/cyclone/fragments/HomeFragment.kt
@@ -1,10 +1,10 @@
 package com.vishal2376.cyclone.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.vishal2376.cyclone.R
 
 class HomeFragment : Fragment() {
