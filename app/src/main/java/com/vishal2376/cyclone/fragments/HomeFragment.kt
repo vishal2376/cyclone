46a3cf34b1e3ed2899405486e6dac3b211fc2df3
Vishal Singh <vishalsingh2376@gmail.com>
2023-04-11 18:59:10 +0530

    button click navigation done

--- app/src/main/java/com/vishal2376/cyclone/fragments/HomeFragment.kt
+++ app/src/main/java/com/vishal2376/cyclone/fragments/HomeFragment.kt
@@ -4,17 +4,44 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.vishal2376.cyclone.R
+import com.vishal2376.cyclone.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.layoutHelp.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_helpFragment)
+        }
+
+        binding.layoutPrecautions.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_precautionsFragment)
+        }
+
+        binding.layoutMap.setOnClickListener {
+            Toast.makeText(requireContext(), "Implementing soon", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
--- app/src/main/java/com/vishal2376/cyclone/fragments/SignUpFragment.kt
+++ app/src/main/java/com/vishal2376/cyclone/fragments/SignUpFragment.kt
@@ -5,17 +5,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.vishal2376.cyclone.R
+import com.vishal2376.cyclone.databinding.FragmentSignUpBinding
 
 
 class SignUpFragment : Fragment() {
 
+
+    private var _binding: FragmentSignUpBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+        _binding = FragmentSignUpBinding.inflate(inflater, container, false)
+
+        return binding.root
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
