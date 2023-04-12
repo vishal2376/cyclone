28f5dbc3b12c2364093978f94ae5862a1228546a
Vishal Singh <vishalsingh2376@gmail.com>
2023-04-12 22:01:27 +0530

    firebase auth implemented : signup and login done

--- app/src/main/java/com/vishal2376/cyclone/fragments/LoginFragment.kt
+++ app/src/main/java/com/vishal2376/cyclone/fragments/LoginFragment.kt
@@ -4,8 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.vishal2376.cyclone.R
 import com.vishal2376.cyclone.databinding.FragmentLoginBinding
 
@@ -13,6 +17,10 @@ class LoginFragment : Fragment() {
 
     private var _binding: FragmentLoginBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var auth: FirebaseAuth
+
+    private var isValidForm = false
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
@@ -26,12 +34,58 @@ class LoginFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+
+
         binding.btnLogin.setOnClickListener {
-            findNavController().navigate(R.id.action_loginFragment_to_homeFragment)
+
+            val email = binding.editEmail.text.toString()
+            val password = binding.editPassword.text.toString()
+
+            isValidForm = checkForm(email, password)
+
+            if (isValidForm) {
+                loginUser(email, password)
+            } else {
+                Toast.makeText(requireContext(), "Fill Details correctly", Toast.LENGTH_SHORT)
+                    .show()
+            }
         }
 
         binding.tvCreateAccount.setOnClickListener {
             findNavController().navigate(R.id.action_loginFragment_to_signUpFragment)
+        }
+    }
+
+    private fun loginUser(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(requireActivity()) { task ->
+                if (task.isSuccessful) {
+                    val user = auth.currentUser
+                    findNavController().navigate(R.id.action_loginFragment_to_homeFragment)
+                } else {
+                    Toast.makeText(requireContext(), "Authentication failed.", Toast.LENGTH_SHORT)
+                        .show()
+                }
+            }
+    }
+
+    private fun checkForm(email: String, password: String): Boolean {
+        if (email.isNotEmpty() && password.isNotEmpty()) {
+            return true
+        }
+        return false
+    }
+
+
+    override fun onStart() {
+        super.onStart()
+        val currentUser = auth.currentUser
+
+        //if already logged in then navigate to home fragment
+        if (currentUser != null) {
+            findNavController().navigate(R.id.action_loginFragment_to_homeFragment)
         }
     }
 
--- app/src/main/java/com/vishal2376/cyclone/fragments/SignUpFragment.kt
+++ app/src/main/java/com/vishal2376/cyclone/fragments/SignUpFragment.kt
@@ -4,12 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.vishal2376.cyclone.R
 import com.vishal2376.cyclone.databinding.FragmentSignUpBinding
 
 
 class SignUpFragment : Fragment() {
 
+    private lateinit var auth: FirebaseAuth
 
     private var _binding: FragmentSignUpBinding? = null
     private val binding get() = _binding!!
@@ -21,6 +28,41 @@ class SignUpFragment : Fragment() {
         _binding = FragmentSignUpBinding.inflate(inflater, container, false)
 
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+
+        binding.btnCreateAccount.setOnClickListener {
+
+            val name = binding.editName.text.toString()
+            val email = binding.editEmail.text.toString()
+            val password = binding.editPassword.text.toString()
+
+            if (name.isNotEmpty() && email.isNotEmpty() && password.isNotEmpty()) {
+                registerUser(email, password)
+            } else {
+                Toast.makeText(requireContext(), "Fill Details correctly", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
+
+    }
+
+    private fun registerUser(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(requireActivity()) { task ->
+                if (task.isSuccessful) {
+                    val user = auth.currentUser
+                    findNavController().navigate(R.id.action_signUpFragment_to_homeFragment)
+                } else {
+                    Toast.makeText(requireContext(), "Authentication failed.", Toast.LENGTH_SHORT)
+                        .show()
+                }
+            }
     }
 
     override fun onDestroyView() {
